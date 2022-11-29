309f7ab16a18ac49c2540e6d42aca0cc5742bd6b
babar <bapt09.rousseau@gmail.com>
2022-11-29 16:50:56 +0100

    reworked notation

--- isbwt.py
+++ isbwt.py
@@ -4,12 +4,12 @@ import numpy as np
 class sabwt():
     def __init__(self, s):
         self.s = np.array(list(s))
-        self.stypes, self.stypes_substrings = self.suffix_types()
+        self.stypes, self.LMS, self.LMS_substrings = self.suffix_types()
 
     def suffix_types(self):
         types = np.empty(len(self.s), dtype="|S2")
         types[-1] = "S"
-        stypes_index = []
+        LMS = []
         i = len(self.s) - 2
         while i >= 0:
             if self.s[i] < self.s[i+1] or (self.s[i] == self.s[i+1] and types[i+1] == b"S"):
@@ -17,24 +17,24 @@ class sabwt():
             else:
                 types[i] = "L"
                 if types[i+1] == b"S":
-                    types[i+1] = "S*"
-                    stypes_index.append(i+1)
+                    LMS.append(i+1)
             i -= 1
-        stypes_index.sort()
-        stypes_substrings = []
-        for i in range(len(stypes_index) - 1):
-            stypes_substrings.append(
-                self.s[stypes_index[i]:(stypes_index[i+1] + 1)])
-        return types, stypes_substrings
+        LMS.sort()
+        LMS_substrings = []
+        for i in range(len(LMS) - 1):
+            LMS_substrings.append(
+                self.s[LMS[i]:(LMS[i+1] + 1)])
+        return types, LMS, LMS_substrings
 
     def induce(self):
-        for i in range(len(self.stypes_substrings)):
-            u = reversed(self.stypes_substrings[i])
+        for i in range(len(self.LMS_substrings)):
+            u = reversed(self.LMS_substrings[i])
             c = u.pop()
 
 
 if __name__ == "__main__":
-    s = "mmississiippii$"
+    s = "mmiissiissiippii$"
     b = sabwt(s)
     print(b.stypes)
-    print(b.stypes_substrings)
+    print(b.LMS)
+    print(b.LMS_substrings)
